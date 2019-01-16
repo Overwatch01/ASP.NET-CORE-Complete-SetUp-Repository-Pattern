80564143ae93cf7849fea8ce8eddebbcacf9a3a8
Foli Dominic <Folidominic97@gmail.com>
2019-01-17 00:24:43 +0100

    Added API to the project

---
+++ API/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationService.Repository.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly ITestAppService _testAppService;
+
+        public TestController(ITestAppService testAppService)
+        {
+            _testAppService = testAppService;
+        }
+
+        [HttpGet("GetAllTests")]
+        public IActionResult GetAllTests()
+        {
+            var result = _testAppService.GetAllTests();
+            return Ok(result);
+        }
+
+        [HttpGet("GetTestById/{id}")]
+        public IActionResult GetAllTests([FromRoute] int id)
+        {
+            var result = _testAppService.GetTestById(id);
+            return Ok(result);
+        }
+    }
+}
